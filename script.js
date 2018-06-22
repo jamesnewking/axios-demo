058ba1cc1761647e16f501030e625e346392b0a7
James Wang <jamesnewking@gmail.com>
2018-06-22 12:12:58 -0700

    full functional axios commands

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 const BASE_URL = 'http://api.reactprototypes.com';
-const API_KEY = '?key=testuser1234';
+const API_KEY = '?key=c318demouser';//query request starts with ? then key/value pair
 
-axios.get(`${BASE_URL}/todos${API_KEY}`).then(resp => {
-    const { todos } = resp.data;
-    const table = $('table tbody');
+//get list of all todo_items
+axios.get(`${BASE_URL}/todos${API_KEY}`).then( getTodoData );
 
-    addToDom(todos, table);
-});
-
-const addToDom = (list, container) => {
-    const tableRows = list.map((item, index) => {
-
-        const tableData = [
-            $(`<td>${index + 1}</td>`),
-            $(`<td>${item.title}</td>`),
-            item.complete ? $(`<td class="text-success">Yes</td>`) : $(`<td class="text-danger">No</td>`)
-        ];
-
-        return $('<tr>').append(tableData);
-    });
-
-    container.append(tableRows);
+function getTodoData(response){
+    console.log('response: ', response);
 };
+
+//get single todo_item, may have more data
+axios.get(`${BASE_URL}/todos/${idToDelete}${API_KEY}`).then( resp =>{
+     console.log('Add one Item response:', resp);
+ });
+
+// const newItem = {
+//     title: 'James W',
+//     details:'When do we see this?'
+// };
+// add new todo_item
+// axios.post(`${BASE_URL}/todos${API_KEY}`,newItem).then( resp =>{
+//     console.log('Add Item response:', resp);
+// });
+//
+// const idToDelete = '5b2d4577e3e0f45f2ed5ca26';
+//
+// delete todo_item
+// axios.delete(`${BASE_URL}/todos/${idToDelete}${API_KEY}`).then(
+//     resp => {
+//         console.log('deleted:', resp);
+//     });
+//
+// toggle complete to todo_item
+
+// axios.put(`${BASE_URL}/todos/${idToToggle}${API_KEY}`).then(
+//     resp => {
+//         console.log('deleted:', resp);
+//     });
